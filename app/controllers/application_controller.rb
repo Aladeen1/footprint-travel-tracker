74a58392a242544b46117f6922f2677c94f208ba
Valentin Raille <Valrail78@gmail.com>
2021-01-15 23:28:33 +0100

    added name to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
 	include Pundit
-	
+	before_action :configure_permitted_parameters, if:  :devise_controller?
+
 	protected
+
+	def configure_permitted_parameters
+    	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  	end
+
 	def authenticate_user!
 		if user_signed_in?
 			super
@@ -11,3 +17,7 @@ class ApplicationController < ActionController::Base
 	end
 	
 end
+
+
+
+
