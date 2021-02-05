2343abf641a2f5807c4c8f700172db4a52c90242
Valentin Raille <Valrail78@gmail.com>
2021-02-06 00:46:17 +0100

    Added user suggestion form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	def show
 		# policies = policy_scope(User)
+		@user_suggestion = current_user
 		@rankings = Ranking.all
 		@user = User.find(params[:id])
 		@flights = @user.flights
@@ -18,6 +19,21 @@ class UsersController < ApplicationController
 		@user.save!
 
 		@airports = Flight.select("flights.id as flight_id", :city, :longitude, :latitude).joins("INNER JOIN airports ON (flights.iata_departure = airports.iata_code OR flights.iata_arrival = airports.iata_code)").where(user_id: @user.id).order("flight_id ASC")
-
 	end
-end
+
+	def update
+		@user = current_user
+		@user.update(user_params)
+
+		redirect_to user_path(User.find(params[:id]))
+
+		flash[:alert] = "Thank you for your suggestion!"
+	end
+
+	private 
+
+	def user_params
+		params.require(:user).permit(:suggestion)
+	end
+end 
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'flights#new'
   resources :flights, only: [:new, :create, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:show,:update]
   resources :rankings, only: [:new, :show, :create]
   resources :contest_registrations, only: [:create, :destroy]
 end
